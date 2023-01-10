7fca3159fa4e1ae7f2554e053269c1b4ade1fbac
Elie Elmalem <elie.elmalem@gmail.com>
2023-01-10 12:51:08 +0000

    Code looking neater with Pipeline module

--- B1/face_shape_detection.py
+++ B1/face_shape_detection.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from sklearn import decomposition
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
 import numpy as np
 
 def get_data():
@@ -20,33 +21,48 @@ def get_data():
 
     return tr_X, tr_Y, te_X, te_Y
 
+def predict(tr_X, tr_Y, te_X, te_Y):
+
+    # print('Starting PCA')
+    # pca = decomposition.PCA(n_components=150, whiten=True)
+    # pca.fit(tr_X)
+
+    # print('Transforming data')
+    # X_train_pca = pca.transform(tr_X)
+    # X_test_pca = pca.transform(te_X)
+
+    # print('Fitting model')
+    # clf = svm.SVC(C=5., gamma=0.001)
+    # clf.fit(X_train_pca, tr_Y)
+
+    # print('Predicting')
+    # pred = clf.predict(X_test_pca)
+    # # pred = softReg.predict(X_test_pca)
+
+    # Testing SVM
+    clf = Pipeline([('pca', decomposition.PCA(n_components=150, whiten=True)),
+                    ('svm', svm.SVC(C=5., gamma=0.001))])
+
+    clf.fit(tr_X, tr_Y)
+    pred = clf.predict(te_X)
+
+    print("SVM Accuracy:", accuracy_score(te_Y, pred))
+    print('SVM Classification report \n', classification_report(te_Y, pred, zero_division=0))
+
+    #Testing softmax regression
+    softReg = Pipeline([('pca', decomposition.PCA(n_components=150, whiten=True)),
+                        ('logistic', LogisticRegression(multi_class = 'multinomial', solver = 'lbfgs'))])
+
+    softReg.fit(tr_X, tr_Y)
+    soft_pred = softReg.predict(te_X)
+
+    print("SoftMax Accuracy:", accuracy_score(te_Y, soft_pred))
+    print('SoftMax Classification report \n', classification_report(te_Y, soft_pred, zero_division=0))
+
 def test_face_shape_detection():
     tr_X, tr_Y, te_X, te_Y= get_data()
 
     tr_X = tr_X.reshape((tr_X.shape[0], 64*64))
     te_X = te_X.reshape((te_X.shape[0], 64*64))
 
-    print('Starting PCA')
-    pca = decomposition.PCA(n_components=150, whiten=True)
-    pca.fit(tr_X)
-
-    print('Transforming data')
-    X_train_pca = pca.transform(tr_X)
-    X_test_pca = pca.transform(te_X)
-
-    print('Fitting model')
-    # clf = svm.SVC(C=5., gamma=0.001)
-    # clf.fit(X_train_pca, tr_Y)
-
-    softReg = LogisticRegression(multi_class = 'multinomial', solver = 'lbfgs')
-    softReg.fit(X_train_pca, tr_Y)
-
-    print('Predicting')
-    # pred = clf.predict(X_test_pca)
-    pred = softReg.predict(X_test_pca)
-
-    print("Accuracy:", accuracy_score(te_Y, pred))
-    print('Classification report \n', classification_report(te_Y, pred, zero_division=0))
-
-
-    
+    predict(tr_X, tr_Y, te_X, te_Y)
